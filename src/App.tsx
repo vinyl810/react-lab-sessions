8e9a1ba504effc8693e21376e5e2101549a543e3
daewon-koast <daewon.kim@koast.net>
2024-09-23 09:15:06 +0900

    fix: LAB2 description fix

--- src/App.tsx
+++ src/App.tsx
@@ -57,7 +57,7 @@ function App() {
            *  - className: 'grid grid-cols-[40px,_1fr,_60px] justify-items-center border-b border-gray-300 py-2'로 지정합니다.
            * 6. 할 일 목록의 각 요소는 다음과 같은 내용을 **순서대로** 표시합니다.
            *  - 별 버튼: isFavorite 속성이 true인 경우 active-star, false인 경우 star인 StyledButton 버튼
-           *  - 할 일 제목: title 속성
+           *  - 할 일 제목: title 속성 (plain text로 표시합니다. p 요소등의 html 태그로 감싸지 않습니다.)
            *  - 삭제 버튼: '삭제' 텍스트를 가진 StyledButton 버튼 (red 속성)
            * 7. 별 버튼을 클릭하면 관심목록에 추가됩니다. 다시 클릭하면 관심목록에서 제거됩니다.
            * 8. 삭제 버튼을 클릭하면 해당 할 일을 목록에서 삭제합니다.
